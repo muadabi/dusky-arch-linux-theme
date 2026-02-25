b18179a98edf78af9e74da7ef3f1621fdac0aecf
dusk <dusk1@myyahoo.com>
2026-02-26 04:51:15 +0530

    commented file manger state

--- user_scripts/update_dusky/update_dusky.sh
+++ user_scripts/update_dusky/update_dusky.sh
@@ -128,7 +128,7 @@ declare -ra UPDATE_SEQUENCE=(
 #    "S | 220_logrotate_optimization.sh"
 #    "S | 225_faillock_timeout.sh"
     "U | 230_non_asus_laptop.sh --auto"
-    "U | 235_file_manager_switch.sh --apply-state"
+#    "U | 235_file_manager_switch.sh --apply-state"
 #    "U | 240_swaync_dgpu_fix.sh --disable"
 #    "S | 245_asusd_service_fix.sh"
 #    "S | 250_ftp_arch.sh"
